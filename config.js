e92f0acc23d275ab8aaeadc58ba22b941122b1fb
Nimesh Tech Hub <154253046+M1nimesh@users.noreply.github.com>
2024-10-26 17:19:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://avatars.githubusercontent.com/u/154253046?s=400&u=23cb08726f9c6e1692ea8e4e676772fcbb09e8cc&v=4",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hi ! I'm NTH-BOT-V1,I'm Alive Now...!",
 };
